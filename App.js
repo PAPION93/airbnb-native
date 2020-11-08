08cdfb6581771a4c04682c889279934a622cb65c
PAPION93 <sjun1319@gmail.com>
2020-11-08 23:02:42 +0900

    Add preloading font, images

--- App.js
+++ App.js
@@ -1,21 +1,43 @@
-import { StatusBar } from "expo-status-bar";
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React, { useState } from "react";
+import { AppLoading } from "expo";
+import { Asset } from "expo-asset";
+import * as Font from "expo-font";
+import { Ionicons } from "@expo/vector-icons";
+import { Text, Image } from "react-native";
+
+const cacheImages = (images) =>
+  images.map((image) => {
+    if (typeof image === "string") {
+      return Image.prefetch(image);
+    } else {
+      return Asset.fromModule(image).downloadAsync();
+    }
+  });
+
+const cacheFonts = (fonts) => fonts.map((font) => Font.loadAsync(font));
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Welcome to Airbnb</Text>
-      <StatusBar style="auto" />
-    </View>
+  const [isReady, setIsReady] = useState(false);
+  const handleFinish = () => setIsReady(true);
+  const loadAssets = async () => {
+    const images = [
+      require("./assets/loginBg.jpeg"),
+      "https://spng.subpng.com/20180404/woq/kisspng-le-case-del-borgantico-apartment-airbnb-renting-im-logo-5ac51890a19698.8834329915228663206619.jpg",
+      // require("./assets/logo.jpeg"),
+    ];
+
+    const fonts = [Ionicons.font];
+    const imagesPromises = cacheImages(images);
+    const fontPromises = cacheFonts(fonts);
+    return Promise.all([...fontPromises, ...imagesPromises]);
+  };
+  return isReady ? (
+    <Text>I'm ready</Text>
+  ) : (
+    <AppLoading
+      onError={console.error}
+      onFinish={handleFinish}
+      startAsync={loadAssets}
+    />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
--- README.md
+++ README.md
@@ -2,7 +2,11 @@
 
 1. `npm -i -g expo-cli`
 2. `expo init airbnb-native`
-    - blank
+   - blank
 3. `expo login`
-    - `enter i` : iOS simulator
+   - `enter i` : iOS simulator
 4. `export start`
+5. `npm i @expo/vector-icons`
+
+expo install expo-asset
+expo install expo-font
