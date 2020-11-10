71edc48800773b6e03d8e3429805f2f6587dd9fe
PAPION93 <sjun1319@gmail.com>
2020-11-11 00:47:43 +0900

    Update welcome screen

--- screens/Welcome.js
+++ screens/Welcome.js
@@ -2,6 +2,7 @@ import React from "react";
 import styled from "styled-components/native";
 import { StatusBar } from "react-native";
 import { BlurView } from "expo-blur";
+import Btn from "../components/Auth/Btn";
 
 const LOGO_URL =
   "https://www.pinclipart.com/picdir/big/52-526160_aribnb-logo-airbnb-logo-no-background-clipart.png";
@@ -21,21 +22,32 @@ const Logo = styled.Image`
   height: 120px;
 `;
 
-export default ({ navigation }) => (
-  <Container>
-    <BlurView
-      intensity={50}
-      tint="light"
-      style={{
-        flex: 1,
-        witdh: "100%",
-        alignItems: "center",
-        justifyContent: "center",
-      }}
-    >
-      <Logo source={{ uri: LOGO_URL }} />
-    </BlurView>
-    <Image source={require("../assets/loginBg.jpeg")} />
-    <StatusBar barStyle="light-content" />
-  </Container>
-);
+const BtnContainer = styled.View``;
+
+export default ({ navigation }) => {
+  const goToSignUp = () => navigation.navigate("SignUp");
+  const goToSignIn = () => navigation.navigate("SignIn");
+
+  return (
+    <Container>
+      <BlurView
+        intensity={50}
+        tint="light"
+        style={{
+          flex: 1,
+          witdh: "100%",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <Logo source={{ uri: LOGO_URL }} />
+        <BtnContainer>
+          <Btn onPress={goToSignUp} text={"Sign Up"} accent={true} />
+          <Btn onPress={goToSignIn} text={"Sign In"} />
+        </BtnContainer>
+      </BlurView>
+      <Image source={require("../assets/loginBg.jpeg")} />
+      <StatusBar barStyle="light-content" />
+    </Container>
+  );
+};
