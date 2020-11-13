4497877e065387be13d839cc521ae656b6502277
PAPION93 <sjun1319@gmail.com>
2020-11-13 15:35:01 +0900

    Add load more

--- screens/Main/Explore/ExplorePresenter.js
+++ screens/Main/Explore/ExplorePresenter.js
@@ -2,36 +2,79 @@ import React from "react";
 import styled from "styled-components/native";
 import { ActivityIndicator } from "react-native";
 import RoomCard from "../../../components/RoomCard";
-import { ScrollView } from "react-native-gesture-handler";
+import { ScrollView, TouchableOpacity } from "react-native-gesture-handler";
+import { LearnMoreLinks } from "react-native/Libraries/NewAppScreen";
 
 const Container = styled.View`
   flex: 1;
   justify-content: center;
   align-items: center;
+  padding-horizontal: 15px;
 `;
 
-export default ({ rooms }) => {
+const FakeBar = styled.View`
+  height: 40px;
+  width: 100%;
+  background-color: white;
+  box-shadow: 1px 5px 5px rgba(200, 200, 200, 0.5);
+  margin: 80px 0px 10px 0px;
+  border-radius: 15px;
+  justify-content: center;
+  padding-left: 10px;
+`;
+
+const FakeText = styled.Text`
+  font-size: 14px;
+  font-weight: 300;
+`;
+
+const LoadMore = styled.View`
+  width: 100%;
+  padding: 10px 10px;
+  align-items: center;
+  background-color: #006a70;
+  border-radius: 5px;
+  margin-bottom: 30px;
+`;
+
+const LoadMoreText = styled.Text`
+  color: white;
+  font-size: 18px;
+  font-weight: 500;
+`;
+
+export default ({ rooms, increasePage }) => {
   return (
     <Container>
       {rooms.length === 0 ? (
         <ActivityIndicator color="black" />
       ) : (
-        <ScrollView
-          showsVerticalScrollIndicator={false}
-          style={{ width: "100%", marginTop: 120 }}
-          contentContainerStyle={{ paddingHorizontal: 15 }}
-        >
-          {rooms.map((room) => (
-            <RoomCard
-              key={room.id}
-              isFav={room.user.is_fav}
-              isSuperHost={room.user.superhost}
-              photos={room.photos}
-              name={room.name}
-              price={room.price}
-            />
-          ))}
-        </ScrollView>
+        <>
+          <FakeBar>
+            <FakeText>search</FakeText>
+          </FakeBar>
+          <ScrollView
+            showsVerticalScrollIndicator={false}
+            style={{ width: "100%" }}
+            contentContainerStyle={{ paddingTop: 30 }}
+          >
+            {rooms.map((room) => (
+              <RoomCard
+                key={room.id}
+                isFav={room.user.is_fav}
+                isSuperHost={room.user.superhost}
+                photos={room.photos}
+                name={room.name}
+                price={room.price}
+              />
+            ))}
+            <TouchableOpacity onPress={increasePage}>
+              <LoadMore>
+                <LoadMoreText>Load More</LoadMoreText>
+              </LoadMore>
+            </TouchableOpacity>
+          </ScrollView>
+        </>
       )}
     </Container>
   );
